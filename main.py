94819b48d0204c310eb1da4fc14de62ea4842b7e
shangkaul <shangkaul@gmail.com>
2020-08-01 23:00:04 +0530

    added code for watermark

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from docx.enum.style import WD_STYLE_TYPE
 from docx.shared import Pt
 from docx.enum.text import WD_LINE_SPACING
 from docx2pdf import convert
+import PyPDF2
 
 import csv
 import os.path
@@ -36,11 +37,37 @@ def createDoc(intern):
 
     content=[]
     name=intern[2]
-    designation="Graphic Designer"
-    para1="This is to certify that "+name+" has done his internship as a "+designation+"   at Inception Wave Pvt. Ltd, from 1st June to 31st July 2020."
-    para2="During the internship, he has closely worked as a part of the Content Development Team. He made valuable contribution towards the publicity and digital marketing  ventures of Inception Wave Pvt. Ltd.."
-    para3="His contribution to creative ideas are being implemented in our social media marketing campaigns. During the internship, he demonstrated good communication and designing skills with a self-motivated attitude to learn new things. His performance exceeded the expectations and was able to complete the assigned tasks successfully on time. "
-    para4="We wish him all the best for his future endeavours."
+    dept=intern[4]
+    gender=intern[5]
+    if gender=="male" or "Male":
+        s="m"
+    elif gender=="female" or "Female":
+        s="f"
+    
+    he={"m":"he","f":"she"}
+    his={"m":"his","f":"her"}
+    him={"m":"him","f":"her"}
+    if dept=="GD":
+        designation="Graphic Designer"
+    elif dept=="HR":
+        designation="HR Intern"
+    elif dept=="Marketing":
+        designation="Digital Marketeer"
+
+    start=intern[6]
+    end=intern[7]
+
+
+    para1="This is to certify that "+name+" has done "+his[s]+" internship as a "+designation+"   at Inception Wave Pvt. Ltd, from "+start+" to "+end
+    if dept=="Marketing":
+        para2="During the internship, "+he[s]+" has closely worked as a part of the Operations team for our product : Grapido, "+he[s]+" made a valuable contribution towards the publicity and digital marketing  ventures of Inception Wave Pvt. Ltd.."
+    elif dept=="GD":
+        para2="During the internship, "+he[s]+" has closely worked as a part of the Design and marketing team for our product : Grapido, "+he[s]+" made a valuable contribution towards the publicity and digital marketing  ventures of Inception Wave Pvt. Ltd.."
+    elif dept=="HR":
+        para2="During the internship, "+he[s]+" has closely worked as a part of the Human Resource team for our product : Grapido, "+he[s]+" made a valuable contribution towards the overall team management of different domains of Inception Wave Pvt. Ltd.."
+        
+    para3="Throughout the internship, "+his[s]+" efforts and dedication towards the task assigned was praiseworthy. During the internship, "+he[s]+" demonstrated good communication and designing skills with a self-motivated attitude to learn new things.Further, "+his[s]+" performance exceeded the expectations and was able to complete the assigned tasks successfully on time. "
+    para4="We wish "+him[s]+" all the best for "+his[s]+" future endeavours."
 
     content.append(para1)
     content.append(para2)
@@ -80,10 +107,40 @@ def rdCSV():
         for intern in data:
             createDoc(intern)
 
+def convert(filename):
+    watermark="Response_PDF/"+filename
+    pdf_file="watermark.pdf"
+    merged_file = "Response_PDF/"+filename+".pdf"
+    input_file = open(pdf_file,'rb')
+    input_pdf = PyPDF2.PdfFileReader(input_file)
+    watermark_file = open(watermark,'rb')
+    watermark_pdf = PyPDF2.PdfFileReader(watermark_file)
+    pdf_page = input_pdf.getPage(0)
+    watermark_page = watermark_pdf.getPage(0)
+    pdf_page.mergePage(watermark_page)
+    output = PyPDF2.PdfFileWriter()
+    output.addPage(pdf_page)
+    merged_file = open(merged_file,'wb')
+    output.write(merged_file)
+    merged_file.close()
+    watermark_file.close()
+    input_file.close()
+
+
+def watermark():
+    directory = os.fsencode("Response_PDF/")
+    for file in os.listdir(directory):
+     filename = os.fsdecode(file)
+     if filename.endswith(".pdf"): 
+        #  path=os.path.join("Response_PDF", filename)
+         convert(filename)
+         
+
 
 def main():
     rdCSV()
-    convertPDF()
+    # convertPDF()
+    # watermark()
 
 
 if __name__=="__main__":
